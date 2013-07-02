6fc1203e97cacb29c22ae3fa598cddd5a08138ce
jleonard <thejohnleonard@gmail.com>
2013-07-02 06:52:29 -0400

    starting file parsing

--- tasks/styleguide-color.js
+++ tasks/styleguide-color.js
@@ -27,12 +27,71 @@ module.exports = function(grunt) {
           
         }else{
           var file = grunt.file.read(filepath);
+          file = removeComments(file);
+          file = file.replace(/ /g,"").replace(/\t/g,"").replace(/\n/g,"");
+          var arr = file.split(";");
+          var len = arr.length;
+         
+          for (var i = 0; i < len; i++) {
+            var key_val = arr[i].split(":");
+            grunt.log.write(key_val[0]);
+          }
+          
+
          
           grunt.file.write(dest,file);
         }
       });
 
     });
+
+    function removeComments(str) {
+ 
+        var uid = '_' + +new Date(),
+            primatives = [],
+            primIndex = 0;
+     
+        return (
+            str
+            /* Remove strings */
+            .replace(/(['"])(\\\1|.)+?\1/g, function(match){
+                primatives[primIndex] = match;
+                return (uid + '') + primIndex++;
+            })
+     
+            /* Remove Regexes */
+            .replace(/([^\/])(\/(?!\*|\/)(\\\/|.)+?\/[gim]{0,3})/g, function(match, $1, $2){
+                primatives[primIndex] = $2;
+                return $1 + (uid + '') + primIndex++;
+            })
+     
+            /*
+            - Remove single-line comments that contain would-be multi-line delimiters
+                E.g. // Comment /* <--
+            - Remove multi-line comments that contain would be single-line delimiters
+                E.g. /* // <-- 
+           */
+            .replace(/\/\/.*?\/?\*.+?(?=\n|\r|$)|\/\*[\s\S]*?\/\/[\s\S]*?\*\//g, '')
+     
+            /*
+            Remove single and multi-line comments,
+            no consideration of inner-contents
+           */
+            .replace(/\/\/.+?(?=\n|\r|$)|\/\*[\s\S]+?\*\//g, '')
+     
+            /*
+            Remove multi-line comments that have a replaced ending (string/regex)
+            Greedy, so no inner strings/regexes will stop it.
+           */
+            .replace(RegExp('\\/\\*[\\s\\S]+' + uid + '\\d+', 'g'), '')
+     
+            /* Bring back strings & regexes */
+            .replace(RegExp(uid + '(\\d+)', 'g'), function(match, n){
+                return primatives[n];
+            })
+        );
+     
+    }
 
   });
 
